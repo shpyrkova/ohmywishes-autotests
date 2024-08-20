9442b2463a964cd03066f81209f5e2fd41f7f9c3
shpyrkova <surkov@company.moisport.ru>
2024-08-20 14:28:48 +0700

    review read ExpectedUserData.json

--- src/test/java/tests/api/UserProfileTests.java
+++ src/test/java/tests/api/UserProfileTests.java
@@ -1,5 +1,6 @@
 package tests.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.qameta.allure.Epic;
 import io.restassured.response.Response;
 import models.lombok.User;
@@ -8,7 +9,11 @@ import models.lombok.api.GetOwnUserInfoResponseBody;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import testdata.ExpectedUserData;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,41 +25,47 @@ public class UserProfileTests extends TestBaseApi {
 
     @Test
     @DisplayName("Получение данных о пользователе (о себе)")
-    void getUserProfileTest() {
+    void getUserProfileTest() throws IOException {
 
         Response response = step("Запросить данные о пользователе", apiClient::getOwnUserInfo);
         User userResponseData = response.as(GetOwnUserInfoResponseBody.class).getItem();
         UserSettings userResponseDataSettings = userResponseData.getSettings();
+        String filePath = "src/test/java/testdata/ExpectedUserData.json";
 
-        User expectedUserData = ExpectedUserData.getExpectedUser();
-        UserSettings expectedUserSettings = expectedUserData.getSettings();
+        try (Reader reader = new InputStreamReader(new FileInputStream(filePath))) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            User expectedUserData = objectMapper.readValue(reader, User.class);
+            UserSettings expectedUserSettings = expectedUserData.getSettings();
 
-        step("Проверить, что основные поля в ответе корректны", () -> {
-            assertThat(response.statusCode()).isEqualTo(200);
-            assertThat(userResponseData.getId()).isEqualTo(expectedUserData.getId());
-            assertThat(userResponseData.getBirthday()).isEqualTo(expectedUserData.getBirthday());
-            assertThat(userResponseData.getFirstname()).isEqualTo(expectedUserData.getFirstname());
-            assertThat(userResponseData.getLastname()).isEqualTo(expectedUserData.getLastname());
-            assertThat(userResponseData.getFullname()).isEqualTo(expectedUserData.getFullname());
-            assertThat(userResponseData.getAbout()).isEqualTo(expectedUserData.getAbout());
-            assertThat(userResponseData.getEmail()).isEqualTo(expectedUserData.getEmail());
-            assertThat(userResponseData.getSex()).isEqualTo(expectedUserData.getSex());
-            assertThat(userResponseData.getUsername()).isEqualTo(expectedUserData.getUsername());
-            assertThat(userResponseData.getAccountType()).isEqualTo(expectedUserData.getAccountType());
-            assertThat(userResponseData.getLegalInfo()).isEqualTo(expectedUserData.getLegalInfo());
-            assertThat(userResponseData.isEmailConfirmed()).isEqualTo(expectedUserData.isEmailConfirmed());
-            assertThat(userResponseData.isPro()).isEqualTo(expectedUserData.isPro());
-            assertThat(userResponseData.getAvatar()).isNotNull();
-        });
+            step("Проверить, что основные поля в ответе корректны", () -> {
+                assertThat(response.statusCode()).isEqualTo(200);
+                assertThat(userResponseData.getId()).isEqualTo(expectedUserData.getId());
+                assertThat(userResponseData.getBirthday()).isEqualTo(expectedUserData.getBirthday());
+                assertThat(userResponseData.getFirstname()).isEqualTo(expectedUserData.getFirstname());
+                assertThat(userResponseData.getLastname()).isEqualTo(expectedUserData.getLastname());
+                assertThat(userResponseData.getFullname()).isEqualTo(expectedUserData.getFullname());
+                assertThat(userResponseData.getAbout()).isEqualTo(expectedUserData.getAbout());
+                assertThat(userResponseData.getEmail()).isEqualTo(expectedUserData.getEmail());
+                assertThat(userResponseData.getSex()).isEqualTo(expectedUserData.getSex());
+                assertThat(userResponseData.getUsername()).isEqualTo(expectedUserData.getUsername());
+                assertThat(userResponseData.getAccountType()).isEqualTo(expectedUserData.getAccountType());
+                assertThat(userResponseData.getLegalInfo()).isEqualTo(expectedUserData.getLegalInfo());
+                assertThat(userResponseData.isEmailConfirmed()).isEqualTo(expectedUserData.isEmailConfirmed());
+                assertThat(userResponseData.isPro()).isEqualTo(expectedUserData.isPro());
+                assertThat(userResponseData.getAvatar()).isNotNull();
+            });
 
-        step("Проверить, что настройки пользователя в ответе корректны", () -> {
-            assertThat(userResponseDataSettings.isPrivateWishesBlurred()).isEqualTo(expectedUserSettings.isPrivateWishesBlurred());
-            assertThat(userResponseDataSettings.isReservedWishesCounterEnabled()).isEqualTo(expectedUserSettings.isReservedWishesCounterEnabled());
-            assertThat(userResponseDataSettings.isSecretSantaAllYearAvailable()).isEqualTo(expectedUserSettings.isSecretSantaAllYearAvailable());
-            assertThat(userResponseDataSettings.getDefaultCurrency()).isEqualTo(expectedUserSettings.getDefaultCurrency());
-            assertThat(userResponseDataSettings.getDefaultLocale()).isEqualTo(expectedUserSettings.getDefaultLocale());
-            assertThat(userResponseDataSettings.getSocialProfiles()).isEqualTo(expectedUserSettings.getSocialProfiles());
-        });
+            step("Проверить, что настройки пользователя в ответе корректны", () -> {
+                assertThat(userResponseDataSettings.isSystemNewsReceiveAllowed()).isEqualTo(expectedUserSettings.isSystemNewsReceiveAllowed());
+                assertThat(userResponseDataSettings.isPrivateWishesBlurred()).isEqualTo(expectedUserSettings.isPrivateWishesBlurred());
+                assertThat(userResponseDataSettings.isReservedWishesCounterEnabled()).isEqualTo(expectedUserSettings.isReservedWishesCounterEnabled());
+                assertThat(userResponseDataSettings.isSecretSantaAllYearAvailable()).isEqualTo(expectedUserSettings.isSecretSantaAllYearAvailable());
+                assertThat(userResponseDataSettings.getDefaultCurrency()).isEqualTo(expectedUserSettings.getDefaultCurrency());
+                assertThat(userResponseDataSettings.getDefaultLocale()).isEqualTo(expectedUserSettings.getDefaultLocale());
+                assertThat(userResponseDataSettings.getSocialProfiles()).isEqualTo(expectedUserSettings.getSocialProfiles());
+            });
+        }
+
     }
 
 }
